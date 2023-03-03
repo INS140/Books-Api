09b4b28adc1cb2edb551b3718fbbfd69a757a7bb
INS140 <IsaacNSaettel@gmail.com>
2023-03-03 05:54:01 -0800

    initial

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+// Connect Mongoose
+mongoose.set({strictQuery: true})
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+}).then(console.log('MONGO CONNECTED'))
+
+// Middleware
+app.use(express.json())
+
+// Routes
+
+app.get('/', (req, res) => {
+    res.json('Hello World!')
+})
+
+// Server Listener
+app.listen(process.env.PORT, () => console.log('SERVER LIVE'))
